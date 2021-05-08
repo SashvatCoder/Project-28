70f510d88911351326450f1392d7fa893920ed06
SashvatCoder <77525587+SashvatCoder@users.noreply.github.com>
2021-05-08 12:01:23 +0530

    Add files via upload

---
+++ tree.js
@@ -0,0 +1,18 @@
+class Tree{
+contructor(x,y,width,height){
+var options={
+isStatic:true
+
+}
+this.body=Bodies.rectangle(x,y,width,height,options)
+this.image=loadImage("images/tree.png")
+this.width=width
+this.height=height
+World.add(world,this.body)
+}
+display(){
+var pos=this.body.position
+imageMode(CENTER)
+image(this.image,pos.x,pos.y,this.width,this.height)
+}
+}
